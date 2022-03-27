583a564ae637d29fc0d2d735b7835840da8853a4
JoO0oss <95944691+JoO0oss@users.noreply.github.com>
2022-03-27 17:15:54 +0100

    Updated __init__.py
    __all__ now reflects changes made in macro.py.

--- snakebinds/__init__.py
+++ snakebinds/__init__.py
@@ -1,4 +1,4 @@
-"""Use this to make keybinds, macros and hotkeys, powered by Python."""
-from snakebinds.macro import bind, unbind
+"""Use this to make keybinds, macros and hotkeys, powered by Python and pynput."""
+from snakebinds.macro import bind, unbind, rebind, is_running
 
-__all__ = ["bind", "unbind"]
+__all__ = ["bind", "unbind", "rebind", "is_running"]
