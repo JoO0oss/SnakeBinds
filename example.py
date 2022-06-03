56a6c7f714768ae3e5dd5e85aa53e8656d41bb11
JoO0oss <95944691+JoO0oss@users.noreply.github.com>
2022-06-03 17:26:32 +0100

    Update example.py
    This now has a few more examples to reflect the new features added with ctl, and has a slightly better example use of aliases.
    (I also forgot to mention in another commit that ^, v, <, > aliases for the arrow keys have gone from .> to $>, and example.py now shows the $ abbreviations in use.)

--- example.py
+++ example.py
@@ -1,20 +1,19 @@
-from snakebinds import bind, unbind
+from snakebinds import bind, unbind, ctl
 import time
-import pyautogui
+
+pyautogui = ctl.import_pyautogui()
+
+pyautogui.PAUSE = 0
+
 
 @bind
 def test_hotkey():
     """ctrl+shift+t"""
     print("Test hotkey runs.")
 
-@bind
-def alternative_test_hotkey():
-    """control+shift+y"""
-    print("Alternative test hotkey runs as well.")
-
 def click_delay():
     """None-macro function, can be ran by the the programmer as normal."""
-    pyautogui.click()
+    pyautogui.click(button="left")
     time.sleep(0.05)
 
 @bind
@@ -24,9 +23,26 @@ def click_hotkey():
         click_delay()
 
 @bind
+def hold_click_hotkey():
+    """alt+m_forwards"""
+    while ctl.is_held("alt+m_forwards"):
+        click_delay()
+
+@bind
+def hscroll_left():
+    """shift+$r"""
+    pyautogui.hscroll(3)
+
+@bind
+def hscroll_right():
+    """shift+$<"""
+    pyautogui.hscroll(-3)
+
+@bind
 def leave_hotkey():
     """ctrl+esc+!shift"""
-    print("Stopping Macros.")
+    print("Finishing up.")
     unbind()
 
+print("Starting Macros.")
 bind()
