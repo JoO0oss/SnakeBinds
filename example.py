4f781cf58069866892499bedce99d5f9e66975bb
JoO0oss <95944691+JoO0oss@users.noreply.github.com>
2022-06-04 12:45:23 +0100

    Updated example.py

--- example.py
+++ example.py
@@ -1,11 +1,8 @@
 from snakebinds import bind, unbind, ctl
 import time
-import pyautogui
-# Sneaky tip, depending on your IDE, this gives you all the nice autocompletes etc. without
-# affecting your code.
 
-pyautogui = ctl.import_pyautogui()  # Replacement threadsafe pyautogui interface.
-pyautogui.PAUSE = 0  # Interacting with constants works identically.
+pyautogui = ctl.import_pyautogui()
+pyautogui.PAUSE = 0
 
 
 @bind
@@ -27,36 +24,29 @@ def click_delay():
 def click_hotkey():
     """ctrl+m_forwards"""
     for _ in range(10):
-        # click_delay() is a blocking function, but will only hold things up in this function.
         click_delay()
 
 @bind
 def hold_click_hotkey():
     """alt+m_forwards"""
-    # ctl.is_held() can be used to run things while the user holds the macro combination down.
     while ctl.is_held("alt+m_forwards"):
         click_delay()
 
 @bind
 def hscroll_right():
     """shift+$r"""
-
-    # Shift + right arrow key
-    pyautogui.hscroll(3)  # Scrolls to the right.
+    pyautogui.hscroll(3)
 
 @bind
 def hscroll_left():
     """shift+$<"""
-
-    # Shift + left arrow key
-    pyautogui.hscroll(-3)  # Scrolls to the left.
+    pyautogui.hscroll(-3)
 
 @bind
 def leave_hotkey():
     """ctrl+esc+!shift"""
     print("Finishing up.")
-    unbind()  # bind() only stops blocking after this call once all macros have finished.
+    unbind()
 
 print("Starting Macros.")
 bind()
-print("Done.")
