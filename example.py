5732780fecb03c1c72db2f92c886be8a18422f2d
JoO0oss <95944691+JoO0oss@users.noreply.github.com>
2022-06-04 12:43:13 +0100

    Updated example.py
    Apparently I don't know right from left. That should now be fixed.

--- example.py
+++ example.py
@@ -1,15 +1,22 @@
 from snakebinds import bind, unbind, ctl
 import time
+import pyautogui
+# Sneaky tip, depending on your IDE, this gives you all the nice autocompletes etc. without
+# affecting your code.
 
-pyautogui = ctl.import_pyautogui()
-
-pyautogui.PAUSE = 0
+pyautogui = ctl.import_pyautogui()  # Replacement threadsafe pyautogui interface.
+pyautogui.PAUSE = 0  # Interacting with constants works identically.
 
 
 @bind
 def test_hotkey():
     """ctrl+shift+t"""
     print("Test hotkey runs.")
+
+@bind
+def alternative_test_hotkey():
+    """control+shift+y"""
+    print("Alternative test hotkey runs as well.")
 
 def click_delay():
     """None-macro function, can be ran by the the programmer as normal."""
@@ -20,29 +27,36 @@ def click_delay():
 def click_hotkey():
     """ctrl+m_forwards"""
     for _ in range(10):
+        # click_delay() is a blocking function, but will only hold things up in this function.
         click_delay()
 
 @bind
 def hold_click_hotkey():
     """alt+m_forwards"""
+    # ctl.is_held() can be used to run things while the user holds the macro combination down.
     while ctl.is_held("alt+m_forwards"):
         click_delay()
 
 @bind
-def hscroll_left():
+def hscroll_right():
     """shift+$r"""
-    pyautogui.hscroll(3)
+
+    # Shift + right arrow key
+    pyautogui.hscroll(3)  # Scrolls to the right.
 
 @bind
-def hscroll_right():
+def hscroll_left():
     """shift+$<"""
-    pyautogui.hscroll(-3)
+
+    # Shift + left arrow key
+    pyautogui.hscroll(-3)  # Scrolls to the left.
 
 @bind
 def leave_hotkey():
     """ctrl+esc+!shift"""
     print("Finishing up.")
-    unbind()
+    unbind()  # bind() only stops blocking after this call once all macros have finished.
 
 print("Starting Macros.")
 bind()
+print("Done.")
